b4e2690a5c1b07f9f7c2e48b0f39cf17eeca1b9c
alicoder123411 <sameersingla240@gmail.com>
2026-01-17 17:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function shortcut(s1, s2) {
+	return s1[0]+s2[0]
   // your code here
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+
